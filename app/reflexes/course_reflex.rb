5a7e01babce244689c66e211c3cb40608aff6b41
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-26 14:05:23 +0000

    Add SR to courses bulk_update

--- app/reflexes/course_reflex.rb
+++ app/reflexes/course_reflex.rb
@@ -21,4 +21,22 @@ class CourseReflex < ApplicationReflex
   #   end
   #
   # Learn more at: https://docs.stimulusreflex.com
+
+  def bulk_update
+    return flash[:alert] = 'Not updated; no courses were selected.' if params[:ids].blank?
+
+    @courses = Course.where(id: params[:ids]).all
+
+    if @courses.update_all(bulk_params.to_h)
+      flash[:notice] = 'Courses successfully updated.'
+    else
+      flash[:alert] = 'An error occurred whilst updating courses.'
+    end
+  end
+
+  private
+
+  def bulk_params
+    params.require(:course).permit(:status)
+  end
 end
