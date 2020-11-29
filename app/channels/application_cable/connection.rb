7b518fc26f6fefc181f0017f704d55a62030558c
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-29 15:13:58 +0000

    Update ApplicationCable identifier

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :session_id
+    identified_by :current_user
 
     def connect
-      self.session_id = request.session.id
+      self.current_user = current_user_via_warden
+    end
+
+    private
+
+    # Returns a user object, or nil
+    def current_user_via_warden
+      env["warden"].user
     end
   end
 end
