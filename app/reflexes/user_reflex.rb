aac058f6b48f47f27e70e7af18e70ee05226077d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-29 13:11:33 +0000

    Add inline updates to admin user index

--- app/reflexes/user_reflex.rb
+++ app/reflexes/user_reflex.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class UserReflex < ApplicationReflex
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = 'User successfully updated.'
+    else
+      flash[:alert] = 'An error occurred while updating User.'
+    end
+  end
+
   def validate
     @user = User.where(id: params[:id]).first_or_initialize
     @user.assign_attributes(user_params)
