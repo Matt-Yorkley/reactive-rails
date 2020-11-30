8760e34105e3f5fe64e1f873524ca5233529993e
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2020-11-30 13:28:04 +0000

    Re-use concern in CourseReflex

--- app/reflexes/course_reflex.rb
+++ app/reflexes/course_reflex.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 
 class CourseReflex < ApplicationReflex
+  include Rest::Courses
+
   before_reflex :authorize_resource!, only: [:bulk_update]
 
   def bulk_update
     return flash[:alert] = 'Not updated; no courses were selected.' if params[:ids].blank?
 
-    @courses = Course.where(id: params[:ids]).all
-
-    if @courses.update_all(bulk_params.to_h)
+    if bulk_update_course
       flash[:notice] = 'Courses successfully updated.'
     else
       flash[:alert] = 'An error occurred whilst updating courses.'
     end
-  end
-
-  private
-
-  def bulk_params
-    params.require(:course).permit(:status)
   end
 end
